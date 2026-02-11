60c359a9913403fe886c51036717eb644e6a94ff
StarHuntingNinja <88210799+StarHuntingNinja@users.noreply.github.com>
2026-02-11 15:11:31 -0600

    Knuckles' dig changes
    - no longer changes his speed
    - can now be parried

--- BattleMod/Lua/3-Functions/3-Player/Special Moves/Lib_ActDig.lua
+++ BattleMod/Lua/3-Functions/3-Player/Special Moves/Lib_ActDig.lua
@@ -84,12 +84,6 @@ B.Action.Dig=function(mo,doaction)
 	local player = mo.player
 	local skin = skins[player.skin]
 	local gray = "\x86"
-	
-	if not (player.gotflag or player.gotcrystal) and player.actionstate == 0 then //For digging state
-		player.normalspeed = skin.normalspeed
-		player.acceleration = skin.acceleration
-		player.thrustfactor = skin.thrustfactor
-	end	
 
 	if P_PlayerInPain(player)
 	or player.playerstate != PST_LIVE
@@ -171,7 +165,7 @@ B.Action.Dig=function(mo,doaction)
 		mo.flags = $|MF_NOCLIPTHING
 		mo.flags2 = $|MF2_DONTDRAW
 		player.pflags = ($|PF_JUMPSTASIS) & ~PF_SPINNING
-		player.normalspeed = skins[mo.skin].normalspeed*3/5
+		--player.normalspeed = skins[mo.skin].normalspeed*3/5
 		S_StartSound(mo,sfx_s3kccs)
 		player.canguard = 0
 		return
@@ -327,8 +321,8 @@ B.Action.Dig=function(mo,doaction)
 	mo.flags2 = $|MF2_DONTDRAW //Invisibility
 	player.charability2 = 0 //Disallow spindashing
 	player.canguard = false //Disallow guarding
-	player.normalspeed = skin.normalspeed*9/8
-	player.acceleration = skin.acceleration*9/8
+--	player.normalspeed = skin.normalspeed*9/8
+--	player.acceleration = skin.acceleration*9/8
 	
 	//Clip to ground
 	if not(climbing) then
@@ -447,7 +441,7 @@ B.Knuckles_PreCollide = function(n1,n2,plr,mo,atk,def,weight,hurt,pain,ground,an
 end
 
 B.Knuckles_Collide = function(n1,n2,plr,mo,atk,def,weight,hurt,pain,ground,angle,thrust,thrust2,collisiontype)
-	if plr[n1].rising == true
+	if plr[n1].rising == true and not plr[n2].guard
 		if not ((hurt == 1 and n1 == 1) or (hurt == -1 and n1 == 2))
 		and not ((hurt == 1 and n2 == 1) or (hurt == -1 and n2 == 2))
 		then
@@ -480,6 +474,8 @@ B.Knuckles_Collide = function(n1,n2,plr,mo,atk,def,weight,hurt,pain,ground,angle
 				return false
 			end
 		end
+	elseif plr[n1].rising == true and plr[n2].guard
+		P_DamageMobj(mo[n2],mo[n1],mo[n1])
 	elseif plr[n1] and mo[n1].health and not(pain[n1])
 		and (plr[n1].pflags&PF_GLIDING or plr[n1].climbing 
 			or (plr[n1].charability == CA_GLIDEANDCLIMB) and collisiontype > 1 and P_IsObjectOnGround(mo[n1]) and not(plr[n1].pflags&PF_SPINNING)
